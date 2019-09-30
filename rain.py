4c53db88ab5c23eabe66581e26fa85cc835409ec
jjin300 <jjin300@gmail.com>
2019-09-30 19:06:47 +0900

    Add files via upload

---
+++ rain.py
@@ -0,0 +1,38 @@
+import pygame, sys, random
+from pygame.locals import *
+
+pygame.init()
+pygame.display.set_caption("rain")
+screen = pygame.display.set_mode((1000, 600))
+clock = pygame.time.Clock()
+raindrop_spawn_time = 0
+
+class Raindrop:
+    def __init__(self):
+        self.x = random.randint(0, 1000)
+        self.y = -5
+        self.speed = random.randint(5, 18)
+
+    def move(self):
+        self.y += self.speed
+
+    def draw(self):
+        pygame.draw.line(screen, (0, 0, 0), (self.x, self.y), (self.x, self.y+5), 1)
+
+
+raindrops = []
+
+while 1:
+    clock.tick(60)
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            sys.exit()
+
+    raindrops.append(Raindrop())
+    screen.fill((255, 255, 255))
+
+    for raindrop in raindrops:
+        raindrop.move()
+        raindrop.draw()
+        
+    pygame.display.update()
